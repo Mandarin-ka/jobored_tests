0c6cb049ed8edf470efd48db2fd301e968da49d9
Evgeniy <jdev03sgo@gmail.com>
2023-05-19 01:10:59 +0300

    add vacancies components, service

--- jobored/src/App.js
+++ jobored/src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import VacancyService from './components/API/VacancyService';
 import Header from './components/Header/Header';
+import { useFetching } from './components/hooks/useFetching';
+import Vacancies from './components/Vacancies/Vacancies';
 import './styles/Main.css'
 
 
 function App() {
+  const [vacancies, setVacancies] = useState([]);
+  const [totalVacancies, setTotalVacancies] = useState(0);
+
+
+  const [fetchVacancies, isVacanciesLoading, vacanciesError] = useFetching(async () => {
+    const response = await VacancyService.getVacancies({keyword: '', page: 1, paymentFrom: null, paymentTo: null, noAgreement: null, catalogues: null});
+    setVacancies(response.data.objects);
+    setTotalVacancies(response.data.total);
+
+  })
+
+  console.log(vacancies)
+
+
+  useEffect(() => {
+    fetchVacancies();
+  }, [])
+
+
+
   return (
-    <div className="App">
+    <div className="main__app">
       <Header/>
+      <Vacancies vacancies={vacancies}/>
     </div>
   );
 }
